bf74be256e032b279023a1ff14d0f4165394312a
oswaldo <oswaldoadidas@hotmail.com>
2019-01-13 15:26:34 -0600

    s

---
+++ src/ejemplosipply/EjemploSipply.java
@@ -0,0 +1,60 @@
+
+package ejemplosipply;
+
+import java.util.Scanner;
+
+
+
+
+public class EjemploSipply {
+
+    
+    public static void main(String[] args) {
+        
+        /*Scanner scanner = new Scanner(System.in);
+        int pin = 1993;
+        
+        
+        do{
+        System.out.println("Ingrese pin para retirar dinero");
+        pin = scanner.nextInt();
+        
+        if(pin == 1993){
+        System.out.println("Bienvenido a BANORTE");
+        
+        }else{
+        System.out.println("PIN incorrecto intente de nuevo");
+        }
+        
+        }while(pin !=1993);*/
+        
+        /* Scanner sc = new Scanner(System.in);
+        int numero=0;
+        int auxiliar=1;
+        System.out.println("Ingresa numero para factorial");
+        numero = sc.nextInt();
+        
+        while(numero>0){
+        
+        auxiliar = auxiliar * numero;
+        numero--;
+        }
+        System.out.println("Resultado es: "+auxiliar);*/
+        
+        /*Scanner sc = new Scanner(System.in);
+        int dividir=0;
+        int dividir2 =0;
+        int resultado=0;
+        do{
+        
+        System.out.println("Ingresa numerador: ");
+        dividir = sc.nextInt();
+        System.out.println("Ingresa denominador: ");
+        dividir2 = sc.nextInt();
+        
+        resultado = dividir / dividir2;
+        System.out.println("Resultado es: "+resultado);
+        }while(resultado>0);*/
+    }
+    
+}
